f562af700a9a86bc3fb22794def694b66bee7500
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-10-13 22:04:10 +0300

    3.8

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('json', (request, _) => {
+    if (request.method === "POST") {
+      return JSON.stringify(request.body)
+  } else {
+    return " "
+  }
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+
 
 const PORT = 3001
 
